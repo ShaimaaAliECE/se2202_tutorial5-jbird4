5cf1b1eb2ac26d1277b352235db79881fb65d57f
Jack Bird <jbird43@uwo.ca>
2021-10-14 17:06:41 -0400

    Check

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 let nextPlayer = 'X'; // takes a value of either 'X' or 'O' according to the game turns
 
 //initialize the game
-
 // use the value stored in the nextPlayer variable to indicate who the next player is
+let TicTacToeGame = new TicTacToeGame();
+
+TicTacToeGame.start();
 
 
 //This call will create the buttons needed for the gameboard.
