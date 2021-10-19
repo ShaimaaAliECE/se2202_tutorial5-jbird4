ecc3e357e48713e4d6ef173e5a4b74f792df5db0
Jack Bird <jbird43@uwo.ca>
2021-10-18 22:04:54 -0400

    checking

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ createGameBoard()
 function createGameBoard()
 {
   
-    let cells = document.querySelectorAll('td');
+    let cells = document.querySelectorAll('#c1');
     let btn = `<button id='btn'>X</button>`;
 
 
